c9ec92926d2bdb5dbcdefca59611a712c66b5c71
sai_greemus <borigamasai2@gmail.com>
2024-10-21 14:08:22 +0530

    comiited

--- firstjava.java
+++ firstjava.java
@@ -5,6 +5,8 @@ public class firstjava {
 
     public static void main(String[] args)
      {
-    System.out.print("wellcome");    
+    System.out.print("wellcome1");    
+    System.out.println("borigama sai");
+    System.out.println("borigama sai1");
     }
 }
